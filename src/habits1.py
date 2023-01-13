2eb00edbc2eb494860c15316afc3b13e9c2e0f2f
Matt Kent <mwkent23@gmail.com>
2023-01-12 23:15:59 -0500

    init

---
+++ src/habits1.py
@@ -0,0 +1,55 @@
+from typing import List, Dict
+
+from chess import Board, Move, PAWN, KNIGHT, BISHOP, ROOK, QUEEN
+
+
+def search(board: Board) -> Move:
+    moves = list(board.legal_moves)
+    sorted_moves = sort_moves(moves)
+    return sorted_moves[0]
+
+
+def sort_moves(board: Board) -> List[Move]:
+    """
+    Priority is:
+    1. Take free pieces
+    2. Take equal trades
+    """
+    moves_to_priorities = get_priority_map(board)
+
+    def priority_getter(value):
+        return moves_to_priorities.get(value)
+
+    return sorted(moves_to_priorities, key=priority_getter)
+
+
+def get_priority_map(board: Board) -> Dict[Move, int]:
+    """
+    Maps moves to their priority.
+    """
+    moves_to_priorities = {}
+    moves = list(board.legal_moves)
+    for move in moves:
+        moves_to_priorities[move] = get_priority(board, move)
+    return moves_to_priorities
+
+
+def get_priority(board: Board, move: Move) -> int:
+    if is_free_capture(board, move):
+        return 0
+    elif is_equal_trade(board, move):
+        return 1
+    else:
+        return 2
+
+
+def is_free_capture(board: Board, move: Move) -> bool:
+    # Is capture and no defenders
+    return board.is_capture(move) and not board.is_attacked_by(not board.turn, move.to_square)
+
+
+def is_equal_trade(board: Board, move: Move) -> bool:
+    piece_types_to_values = {PAWN: 1, KNIGHT: 3, BISHOP: 3, ROOK: 5, QUEEN: 9}
+    return board.is_en_passant(move) or (board.is_capture(move) and
+                                         (piece_types_to_values[board.piece_type_at(move.from_square)] ==
+                                          piece_types_to_values[board.piece_type_at(move.to_square)]))
---
+++ test/test_habits1.py
@@ -0,0 +1,25 @@
+from chess import Board, Move
+
+from src.habits1 import sort_moves, get_priority_map, get_priority
+
+
+def test_sort_moves():
+    board = Board("2kr1bn1/pb1qp3/npP4r/5Pp1/8/5N2/PP3PPP/RNBQKB1R w KQ g6 0 12")
+    moves = sort_moves(board)
+    print(moves)
+    priority_map = get_priority_map(board)
+    print(priority_map)
+
+
+def test_get_priority():
+    board = Board("2kr1bn1/pb1qp3/npP4r/5Pp1/8/5N2/PP3PPP/RNBQKB1R w KQ g6 0 12")
+    expected_to_moves = {
+        1: 'f1a6',
+        1: 'd1d7',
+        1: 'f5g6',
+        0: 'f3g5',
+        0: 'c1g5',
+        2: 'a2a3'
+    }
+    for expected_priority, move in expected_to_moves.items():
+        assert expected_priority == get_priority(board, Move.from_uci(move))
